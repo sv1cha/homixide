85b1de3345a35271c52b67d1bff8ba96179bbb28
sv1cha <metrasverch28@mail.ru>
2024-09-16 20:23:14 +0300

    Update code.h

--- lab1/zadanie2/code.h
+++ lab1/zadanie2/code.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <map>
 
+using namespace std;
+
+class code {
+    private:
+        int key;
+    public:
+        code()=delete;
+        code(int skey);
+        string encrypt(string &text);
+        string decrypt(string &text);
+};

9250904fbc747b91aadd729df0c69d7e1fc0a9f0
sv1cha <metrasverch28@mail.ru>
2024-09-16 20:22:32 +0300

    Update code.cpp

--- lab1/zadanie2/code.cpp
+++ lab1/zadanie2/code.cpp
@@ -1,1 +1,49 @@
+#include <code.h>
 
+code::code(int skey) {
+    key=skey;
+}
+
+string code :: encrypt(string &text) {
+    int k=0;
+    int simvoli=text.size();
+    int stroki=((simvoli-1)/key)+1;
+    char** tabl=new char*[stroki];
+    for (int i=0; i<stroki; i++)
+        tabl[i]=new char[key];
+    for (int i=0; i<stroki; i++)
+        for (int j=0; j<key; j++) {
+            if(k<simvoli) {
+                tabl[i][j]=text[k];
+                k++;
+            }
+        }
+    k=0;
+    for (int j=key-1; j>=0 ; j--)
+        for (int i=0; i<stroki; i++) {
+            text[k]=tabl[i][j];
+            k++;
+        }
+    return text;
+}
+
+string code :: decrypt(string &text) {
+    int k=0;
+    int simvoli=text.size();
+    int stroki=((simvoli-1)/key)+1;
+    char** tabl=new char*[stroki];
+    for (int i=0; i<stroki; i++)
+        tabl[i]=new char[key];
+    for (int j=key-1; j>=0 ; j--)
+        for (int i=0; i<stroki; i++) {
+            tabl[i][j]=text[k];
+            k++;
+        }
+    k=0;
+    for (int i=0; i<stroki; i++)
+        for (int j=0; j<key; j++) {
+            text[k]=tabl[i][j];
+            k++;
+        }
+    return text;
+}
